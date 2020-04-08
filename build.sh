e923824a90e7a2b6a1421fbc7589fabd8d48df53
Phil Evans <phil.evans@thoughtworks.com>
2020-04-08 14:57:08 +0100

    Initial commit

--- build.sh
+++ build.sh
@@ -2,8 +2,15 @@
 
 export TERM=${TERM:-dumb}
 # cp -rv animated-spork jar-files
+echo "Copying input files to output directory"
 cp -rv animated-spork/. /jar-files
+echo "Directory listing of animated-spork"
 ls -al animated-spork
+echo "Directory listing of jar-files"
 ls -al jar-files
+echo "CD"
 cd jar-files
+echo "Current directory is: "
+pwd
+echo "Trying to run gradle:"
 ./gradlew --no-daemon build
