bd6b7718592b9f214a36b555e4b755449421dc3a
Phil Evans <phil.evans@thoughtworks.com>
2020-04-08 15:06:07 +0100

    Initial commit

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 export TERM=${TERM:-dumb}
 # cp -rv animated-spork jar-files
 echo "Copying input files to output directory"
-cp -rv animated-spork/. /jar-files
+cp -r animated-spork/. jar-files
 echo "Directory listing of animated-spork"
 ls -al animated-spork
 echo "Directory listing of jar-files"
